c33989becae11b033e20571d7a690b34a97d5cf3
swu3025 <65374120+swu3025@users.noreply.github.com>
2021-09-25 17:39:52 -0700

    Unit 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Samuel Wu on 9/23/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
